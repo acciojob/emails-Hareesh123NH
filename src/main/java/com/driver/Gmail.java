88ff97e2699ff35a45fbd41df74e14736a9aeb30
Hareesh123NH <nagasarihareesh2005@gmail.com>
2024-05-15 16:05:55 +0530

    clear

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -11,37 +11,28 @@ public class Gmail extends Email {
     /**
      * Mail
      */
-    public class Mail {
-        private Date date;
-        private String sender;
-        private String message;
-        public Mail(Date date,String sender,String message){
-            this.date=date;
-            this.message=message;
-            this.sender=sender;
-        }
-        
-    }
+  
     ArrayList<Mail> inbox;
     ArrayList<Mail> trash;
     public Gmail(String emailId, int inboxCapacity) {
         super(emailId);
         this.inboxCapacity=inboxCapacity;
-        inbox=new ArrayList<>();
-        trash=new ArrayList<>();
+        this.inbox=new ArrayList<>();
+        this.trash=new ArrayList<>();
     }
     public void receiveMail(Date date, String sender, String message){
         // If the inbox is full, move the oldest mail in the inbox to trash and add the new mail to inbox.
         // It is guaranteed that:
         // 1. Each mail in the inbox is distinct.
         // 2. The mails are received in non-decreasing order. This means that the date of a new mail is greater than equal to the dates of mails received already.
+        Mail mail;
         if(inbox.size()==this.inboxCapacity){
-            Mail del=inbox.get(0);
-            trash.add(del);
-            inbox.remove(0);
+            mail=(Mail)this.inbox.get(0);
+            this.trash.add(mail);
+            this.inbox.remove(0);
         }
-        Mail mail=new Mail(date, sender, message);
-        inbox.add(mail);
+        mail=new Mail(date, sender, message);
+        this.inbox.add(mail);
     }
 
     public void deleteMail(String msg){
@@ -53,8 +44,8 @@ public class Gmail extends Email {
         }
         for(Mail m:inbox){
             if(m.message.equals(msg)){
-                trash.add(m);
-                inbox.remove(m);
+                this.trash.add(m);
+               this.inbox.remove(m);
                 return ;
             }
         }
@@ -75,6 +66,7 @@ public class Gmail extends Email {
         if(inbox.isEmpty()){
             return "";
         }
+        //String erf=inbox.get(0).sender;
         return inbox.get(0).message;
     }
 
@@ -92,21 +84,32 @@ public class Gmail extends Email {
 
     public int getInboxSize(){
         // Return number of mails in inbox
-        return inbox.size();
+        return this.inbox.size();
     }
 
     public int getTrashSize(){
         // Return number of mails in Trash
-        return trash.size();
+        return this.trash.size();
     }
 
     public void emptyTrash(){
         // clear all mails in the trash
-        trash.clear();
+        this.trash.clear();
     }
 
     public int getInboxCapacity() {
         // Return the maximum number of mails that can be stored in the inbox
         return this.inboxCapacity;
+    }
+    public class Mail {
+        private Date date;
+        @SuppressWarnings("unused")
+        private String sender;
+        private String message;
+        public Mail(Date date,String sender,String message){
+            this.date=date;
+            this.message=message;
+            this.sender=sender;
+        }
     }
 }
