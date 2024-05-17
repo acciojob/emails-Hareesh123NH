e16192e1461bcfe448d0d1b322c4bd416628a71b
Hareesh123NH <nagasarihareesh2005@gmail.com>
2024-05-17 17:35:57 +0530

    finally

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Email {
 
-    private final String emailId;
+    private String emailId;
     private String password;
 
     public Email(String emailId){
@@ -17,6 +17,12 @@ public class Email {
     public String getPassword() {
         return password;
     }
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public void changePassword(String oldPassword, String newPassword){
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
@@ -25,7 +31,7 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-        if(!oldPassword.equals(this.password)){
+        if(!oldPassword.equals(this.getPassword())){
             System.out.println("Oldpassword is incorrect.");
             return ;
         }
